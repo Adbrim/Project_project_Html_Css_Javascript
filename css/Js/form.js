bca69c69d87c2152eeac1e0e2fbec56e51eebfbd
Adbrim <Adbrim>
2022-10-24 08:16:36 +0000

    ajout du fichier js

---
+++ css/Js/form.js
@@ -0,0 +1,5 @@
+function add(){
+    console.log("hello");
+    const form = document.getElementById('monTableau').value;
+    console.log(form);
+}
---
+++ js/form.js
@@ -0,0 +1,5 @@
+function add(){
+    console.log("hello")
+    let data = document.getElementById("monTableau").onsubmit();
+    console.log(data)
+}
